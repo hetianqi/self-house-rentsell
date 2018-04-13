2cbda2eb8217fbc6278515f55e3948e3c10bd930
hetianqi <449165578@qq.com>
2018-04-13 22:42:42 +0800

    交纳押金

--- pages/pay-deposit/pay-deposit.js
+++ pages/pay-deposit/pay-deposit.js
@@ -5,62 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    loading: false
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+  // 立即支付
+  requestPayment() {
+    var self = this;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    if (this.data.loading) return;
+    
+    self.setData({ loading: true });
+    setTimeout(() => {
+      self.setData({ loading: false });
+    }, 10000);
+    return;
+    wx.request({
+      url: paymentUrl,
+      data: {
+        openid
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log('unified order success, response is:', res)
+        var payargs = res.data.payargs
+        wx.requestPayment({
+          timeStamp: payargs.timeStamp,
+          nonceStr: payargs.nonceStr,
+          package: payargs.package,
+          signType: payargs.signType,
+          paySign: payargs.paySign
+        })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+        self.setData({
+          loading: false
+        })
+      }
+    });
   }
 })
