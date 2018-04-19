cddbb3446f9b7dd6eb09897d48ae46f5f7ca5dfd
hetianqi <449165578@qq.com>
2018-04-19 21:53:53 +0800

    commit

--- components/date-picker/date-picker.js
+++ components/date-picker/date-picker.js
@@ -1,4 +1,7 @@
 // components/date-picker/date-picker.js
+import { formatDate } from '../../libs/util';
+
+
 Component({
   /**
    * 组件的属性列表
@@ -11,7 +14,56 @@ Component({
    * 组件的初始数据
    */
   data: {
+    days: [],
+    today: null,
+    selectedDay: null
+  },
 
+  // 组件实例进入节点树
+  attached() {
+    let now = new Date('2010-2-1');
+    let year = now.getFullYear();
+    let month = now.getMonth();
+    let today = now.getDate();
+
+    // 当月第一天
+    let firstDay = new Date(year, month, 1);
+    // 当月最后一天
+    let lastDay = new Date(year, month + 1, 0);
+    // 第一天是星期几
+    let firstDayOfWeek = firstDay.getDay();
+    console.log(firstDay);
+
+		// 当月总天数
+		let totalDay = lastDay.getDate();
+
+    console.log(firstDayOfWeek, totalDay);
+
+		// 当月日历占几行
+    let totalRows = Math.ceil((totalDay - (7 - firstDayOfWeek)) / 7 + 1);
+    // 当前循环到第几天
+		let currDay = 1;
+    // 日期二维数组，每一维表示一行
+    let days = [];
+    // 每一行具体日期
+    let rowDays;
+
+    console.log(totalRows);
+
+    for (let i = 0; i < totalRows; i++) {	// 行循环
+      rowDays = [];
+      for (let j = 0; j <= 6; j++) {		// 列循环
+        if (i === 0 && j >= firstDayOfWeek || i > 0 && currDay <= totalDay) {
+          rowDays.push(currDay);                              
+          currDay++;
+        } else {
+          rowDays.push(0);
+        }
+      }
+      days.push(rowDays);
+    }
+    
+    console.log(days);
   },
 
   /**
--- libs/util.js
+++ libs/util.js
@@ -1,19 +1,40 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+export const formatNumber = n => {
+  n = n.toString();
+  return n[1] ? n : '0' + n;
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+// 格式化时间参数
+// 参数1： date 日期对象或可转为日期对象的值
+// 参数2： format 字符串，格式化形式，年月日用大写Y、M、D代表，时分秒分别用h、m、s代表，毫秒用S代表
+export function formatDate(date, format) {
+  if (!(date instanceof Date)) {
+      date = new Date(date);
+  }
+  if (isNaN(date.getDate())) {
+      return null;
+  }
 
-module.exports = {
-  formatTime: formatTime
+  format = format || 'yyyy-MM-dd';
+
+  var o = {
+      'M+': date.getMonth() + 1, // 月
+      'd+': date.getDate(), // 天 
+      'h+': date.getHours() % 12, // 小时，12小时制
+      'H+': date.getHours(), // 小时，24小时制 
+      'm+': date.getMinutes(), // 分钟
+      's+': date.getSeconds(), // 秒钟
+      'S': date.getMilliseconds() // 毫秒
+  }
+
+  if (/(y+)/.test(format)) {      //格式化年份
+      format = format.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+
+  for (var k in o) {
+      if (new RegExp('(' + k + ')').test(format)) {
+          format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+      }
+  }
+
+  return format;
 }
