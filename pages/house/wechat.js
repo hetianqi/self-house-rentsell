f3413ffd4cd8b43f3dfeb85ffbc3911f7dd6c706
hetianqi <449165578@qq.com>
2018-05-01 18:08:05 +0800

    微聊模拟

--- pages/house/wechat.js
+++ pages/house/wechat.js
@@ -1,66 +1,85 @@
 // pages/house/wechat.js
 Page({
+  ws: null, // websocket对象
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    historyMessages: [],
+    message: '',
+    scrollIntoId: ''
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--页面加载
    */
-  onLoad: function (options) {
-  
-  },
+  onLoad() {
+    let _this = this;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+    wx.request({
+      url: 'http://192.168.2.103:8000/message',
+      success({ data }) {
+        _this.setData({ historyMessages: data });
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    this.ws = wx.connectSocket({
+      url: 'ws://192.168.2.103:8000',
+      method: 'POST',
+      data: {
+        name: 'heron'
+      }
+    });
+
+    this.ws.onMessage(this.receiveMessage.bind(this));
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  changeMessage(e) {
+    this.setData({ message: e.detail.value });
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+  // 发送消息
+  sendMessage(e) {
+    if (!this.data.message) {
+      wx.showToast({
+        title: '请输入想说的话'
+      });
+      return;
+    }
+    let _this = this;
+    this.ws.send({
+      data: this.data.message,
+      success() {
+        _this.setData({
+          historyMessages: _this.data.historyMessages.concat([{
+            type: 2,
+            content: _this.data.message
+          }]),
+          message: '',
+          scrollIntoId: 'message_' + (_this.data.historyMessages.length + 1)
+        });        
+      }
+    });
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  // 接收消息
+  receiveMessage({ data }) {
+    this.setData({
+      historyMessages: this.data.historyMessages.concat([{
+        type: 1,
+        content: data
+      }]),
+      scrollIntoId: 'message_' + (this.data.historyMessages.length + 1)
+    });
   }
-})
+});
